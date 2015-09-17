a3ad86b89594bcec74fd1101c78983c20366d053
SeanRooney <s00131206@mail.itsligo.ie>
2015-09-17 15:23:29 +0100

    SignalRGame
    Changes

---
+++ MyClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new Connection("http://localhost:52037/Default");
+
+            connection.Received += connection_Received;
+
+            connection.Start().Wait();
+
+            string input = null;
+            //as long as something can be read from console
+            //it will be sent to the server
+            while((input = Console.ReadLine()) != null)
+            {
+                connection.Send(input);
+            }
+        }
+
+        static void connection_Received(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ SignalRGame/Game.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRGame
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public Client Opponent { get; set; }
+        public bool IsPlaying { get; set; }
+        public bool WaitingForMove { get; set; }
+        public bool LookingForOpponent { get; set; }
+
+        public string ConnectionId { get; set; }
+    }
+
+    public class Game : Hub
+    {
+        public static List<Client> _clients = new List<Client>();
+
+        private object _syncRoot = new object();
+
+        public void RegisterClient(string data)
+        {
+            lock(_syncRoot)
+            {
+                var client = _clients
+                    .FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+                if (_clients == null)
+                {
+                    client = new Client { ConnectionId = Context.ConnectionId, Name = data };
+                    _clients.Add(client);
+                }
+
+                client.IsPlaying = false;
+            }
+
+            Clients.Client(Context.ConnectionId).registerComplete();
+        }
+    }
+}
